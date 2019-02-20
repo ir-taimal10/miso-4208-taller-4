20c346ec78c8183bac04e9eea3dc1b0098e443a2
irtaimal <ir.taimal10@uniandes.edu.co>
2019-02-19 23:11:10 -0500

    Configure gremlins species

--- gremlins/specs/gremlins-test.js
+++ gremlins/specs/gremlins-test.js
@@ -1,5 +1,5 @@
 function loadScript(callback) {
-    var s = document.createElement('script');
+    const s = document.createElement('script');
     s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
     if (s.addEventListener) {
         s.addEventListener('load', callback, false);
@@ -15,14 +15,62 @@ function unleashGremlins(ttl, callback) {
         callback();
     }
 
-    var horde = window.gremlins.createHorde();
+    let clicker = null;
+    let formFiller = null;
+    let toucher = null;
+    let scroller = null;
+
+    if (gremlins) {
+        clicker = gremlins.species.clicker().clickTypes(['click']);
+        clicker.canClick(function (element) {
+            return (element.tagName === "A" || element.tagName === "BUTTON") && !element.hidden;
+        });
+
+        formFiller = gremlins.species.formFiller();
+        formFiller.canFillElement(function (element) {
+            console.log(element.type);
+            return (
+                element.tagName === "TEXTAREA" ||
+                element.type === "text" ||
+                element.type === "password" ||
+                element.type === "email")
+                && !element.hidden;
+        });
+
+        toucher = gremlins.species.toucher();
+        toucher.canTouch(function (element) {
+            return !element.hidden;
+        });
+
+        scroller = gremlins.species.scroller();
+
+    }
+    const horde = gremlins.createHorde()
+        .gremlin(clicker)
+        .gremlin(formFiller)
+        .gremlin(toucher)
+        .gremlin(scroller);
+
     horde.seed(1234);
+
+    horde.strategy(gremlins.strategies.distribution()
+        .delay(1)
+        .distribution([0.7, 0.1, 0.1, 0.1])
+    );
 
     horde.after(callback);
     window.onbeforeunload = stop;
     setTimeout(stop, ttl);
     horde.unleash();
 }
+
+browser.waitForReadyStateEx = function (state, timeout) {
+    return browser.waitUntil(function () {
+        return state === browser.execute(function () {
+            return document.readyState;
+        }).value;
+    }, timeout);
+};
 
 describe('Monkey testing with gremlins ', function () {
 
